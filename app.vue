946bc1ebecb92694ab673dd22b13427038466f18
Aleksey Komov <lexxyar@ya.ru>
2024-09-12 20:49:12 +0300

    initial commit NUXT + tailwind

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Hello from "~/components/Hello.vue";
+</script>
+<template>
+  <div>
+    <!--    <NuxtRouteAnnouncer />-->
+    <!--    <NuxtWelcome />-->
+    <Hello />
+  </div>
+</template>
