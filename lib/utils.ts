ea5adef2392452dc26391d5a436a0f6ee0383fea
Aleksey Komov <lexxyar@ya.ru>
2024-09-20 16:18:56 +0300

    bug fix readme

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,18 +4,3 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export const setServerValidationErrors = (error: any, form: UseFormReturn<any>) => {
-  toast.error(error.response?.data?.message as string)
-  let focusField: string = ''
-  if (error.response) {
-    Object.keys(error.response.data.errors).map((field: string) => {
-      if (error.response) {
-        const message: string = error.response.data.errors[field][0]
-        form.setError((field as any), {type: 'server', message})
-      }
-      if (!focusField) focusField = field
-    })
-  }
-  form.setFocus(focusField as any)
-}
